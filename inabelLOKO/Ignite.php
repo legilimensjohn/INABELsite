4911cd7fa859dc6b54df04f95cec4389d38d44a2
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-06-28 22:48:14 +0800

    Update Ignite.php

--- inabelLOKO/Ignite.php
+++ inabelLOKO/Ignite.php
@@ -1,1 +1,47 @@
-
+<!DOCTYPE php>
+<php lang='en'>
+    <head>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Poiret+One&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Zilla+Slab+Highlight&display=swap" rel="stylesheet">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css?family=Muli:900&display=swap" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styleX.css">
+        <script src="project.js"></script>
+        <title>Ignite | ABELoco </title>
+    </head>
+  <body> 
+    <style>
+        #main h2{
+            color: white;
+            font-size: 18px;
+        }
+        #main {
+            padding: 50px 0px 60px 0px;
+        }
+    </style>
+    <header>
+        <div class="container">
+            <h1><span class="high">AbelLux</span> INABEL ILOCO Essence</h1>
+            <div id="brand">
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="History.php">History</a></li>
+                        <li class="cur"><a href="Events.php">Events</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <ul class="donate">
+            <li style="margin-top: 10px; text-align: right;">  
+                <a href="../Project/signup.php" target="_blank"><button class="button"><span>Donate Now</span></button></a>
+            </li> 
+        </ul>     
+    </header>
+ 
