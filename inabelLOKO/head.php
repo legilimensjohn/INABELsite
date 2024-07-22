8293f3cc8fdaabf86696fec7e114abdc91850caf
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-07-22 20:28:16 +0800

    Update head.php

---
+++ inabelLOKO/head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Muli:wght@900&display=swap" rel="stylesheet">>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Homepage | AbelLux INABEL ILOCO Essence </title>
+        <link rel="stylesheet" href="styleX.css">
+        <script src="project.js"></script>
+    </head>
